fbfafb9decaa3a857f4d5c374fbeb05227e9517d
martyniak-roman <roman.martyniakdev@gmail.com>
2026-05-18 15:17:28 +0300

    finished task gi01MZ6v

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css'
+import {coursesTitleArray} from "./data/course.ts";
 
 function App() {
   return (
     <>
+      <ul>
+          {
+            coursesTitleArray.map((value: string, index: number)=>
+              <li className="text-3xl font-medium" key={index}>{value}</li>
+            )
+          }
+      </ul>
     </>
   )
 }
