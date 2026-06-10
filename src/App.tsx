fa7e2175081a9586c44746be489eb96a592eae1b
martyniak-roman <roman.martyniakdev@gmail.com>
2026-06-11 00:20:52 +0300

    finish cf69cHp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.tsx";
 
 function App() {
+  const users =
+      useFetch<{ id: number, name: string }[]>('https://jsonplaceholder.typicode.com/users');
+
+
   return (
-    <>
-    </>
-  )
+      <>
+        {
+            users &&
+            users.map((user) => (
+                <div key={user.id}>
+                  {user.id}: {user.name}
+                </div>
+            ))
+        }
+
+      </>
+  );
 }
 
 export default App
