4ed5e11608843633f9bcb46b96eeaa0d42883ffe
Roman Martyniak <roman-martyniak@192.168.1.7>
2026-06-18 00:35:47 +0300

    finish lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Users} from "./components/Users/Users.tsx";
 
 function App() {
+  console.log('app');
   return (
     <>
       <Users />
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,12 +1,13 @@
 import {useEffect, useState} from "react";
 import {User} from "../User/User.tsx";
 import {getUsers} from "../../services/api.service.ts";
+import type { IUser } from "../../models/IUser.ts";
 
 export const Users = () => {
     console.log('users');
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState<IUser[]>([]);
     useEffect(() => {
-        getUsers().then(setUsers());
+        getUsers().then(setUsers).then(() => console.log('users loaded'));
 
         return () => {
             console.log('unsubscribe');
@@ -17,7 +18,7 @@ export const Users = () => {
     return (
         <div>
             {users.map(user => (
-                <User key={user.id}/>
+                <User key={user.id} user={user} />
             ))}
 
         </div>
