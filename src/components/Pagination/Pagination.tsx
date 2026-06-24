69726fa153aedee1986271de86af083d201df9ee
martyniak-roman <roman.martyniakdev@gmail.com>
2026-06-24 17:07:24 +0300

    add disables buttons

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,11 +2,15 @@ import {useSearchParams} from "react-router-dom";
 
 export const Pagination = () => {
     const [searchParams, setSearchParams] = useSearchParams({page: '1'});
+
+    const minPage: number = 1;
+    const maxPage: number = 7
+
     let currentPage = Number(searchParams.get('page') || '1')
     return (
         <div>
-            <button onClick={() => {setSearchParams({page: (--currentPage).toString()})}}>prev</button>
-            <button onClick={() => {setSearchParams({page: (++currentPage).toString()})}}>next</button>
+            <button onClick={() => {setSearchParams({page: (--currentPage).toString()})}} disabled={currentPage <= minPage}>prev</button>
+            <button onClick={() => {setSearchParams({page: (++currentPage).toString()})}} disabled={currentPage >= maxPage}>next</button>
         </div>
     );
 };
